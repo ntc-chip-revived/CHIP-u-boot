dc02b39d90eda79ee42828b5e6531c5137746b00
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-03-30 10:02:56 +0200

    mtd: nand: toshiba: add correct pairing scheme for tc58teg chips
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/nand_toshiba.c
+++ drivers/mtd/nand/nand_toshiba.c
@@ -59,6 +59,7 @@ static int tc58teg_init(struct mtd_info *mtd, const uint8_t *id)
 
 	chip->setup_read_retry = tc58teg_setup_read_retry;
 	chip->read_retries = 10;
+	mtd->pairing = &dist3_pairing_scheme;
 
 	return 0;
 }
